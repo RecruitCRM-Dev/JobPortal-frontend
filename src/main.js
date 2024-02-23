e8952ad835649e6d75840f0f7dd900bb4f201f26
Divyanshu <divyanshu.20224@knit.ac.in>
2024-02-23 16:21:04 +0530

    Auth Part & User Dashboard   - Login & Register for Candidates   - Login & Register for Employers   - User profile Update Page

--- src/main.js
+++ src/main.js
@@ -8,7 +8,15 @@ import router from './router'
 
 const app = createApp(App)
 
+// import { createVuetify } from 'vuetify'
+// import * as components from 'vuetify/components'
+// import * as directives from 'vuetify/directives'
+
+// const vuetify = createVuetify({
+//   components,
+//   directives,
+// })
 app.use(createPinia())
 app.use(router)
-
+// app.use(vuetify)
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AboutViewVue from '@/views/AboutView.vue'
+import CandidateLoginVue from '@/views/Auth/CandidateLogin.vue'
+import CandidateRegisterVue from '@/views/Auth/CandidateRegister.vue'
+import EmployerLoginVue from '@/views/Auth/EmployerLogin.vue'
+import EmployerRegisterVue from '@/views/Auth/EmployerRegister.vue'
+import UserProfileVue from '@/views/UserProfile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
     {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutViewVue
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: CandidateLoginVue
+    },
+    {
+      path: '/register',
+      name: 'candidate-register',
+      component: CandidateRegisterVue
+    },
+    {
+      path: '/employer/login',
+      name: 'employer-login',
+      component: EmployerLoginVue
+    },
+    {
+      path: '/employer/register',
+      name: 'employer-register',
+      component: EmployerRegisterVue
+    },
+    {
+      path: '/user',
+      name: 'user-page',
+      component: UserProfileVue
     }
   ]
 })
