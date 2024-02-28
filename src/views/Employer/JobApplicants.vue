d0c7f69a53638d448e2a455d8bf40b4ffe6d352d
ishmeetk01 <ishmeet0105@gmail.com>
2024-02-29 03:22:25 +0530

    Api integration for list of applicants

--- src/views/Employer/JobApplicants.vue
+++ src/views/Employer/JobApplicants.vue
@@ -19,7 +19,10 @@
           </tr>
         </thead>
         <tbody class="bg-white">
-          <tr class="text-gray-700">
+          <tr 
+          v-for="applicant in applicants"
+            :key="applicant"
+          class="text-gray-700">
             <td class="px-4 py-3 border">
               <div class="flex items-center text-sm">
                 <div class="relative w-8 h-8 mr-3 rounded-full md:block">
@@ -27,224 +30,24 @@
                   <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
                 </div>
                 <div>
-                  <p class="font-semibold text-black">Sufyan</p>
-                  <p class="text-xs text-gray-600">Developer</p>
+                  <p class="font-semibold text-black">{{applicant.user.name}}</p>
+                  <p class="text-xs text-gray-600">{{applicant.user.role}}</p>
                 </div>
               </div>
             </td>
-            <td class="px-4 py-3 text-ms font-semibold border">sufyan@example.com</td>
+            <td class="px-4 py-3 text-ms font-semibold border">{{applicant.user.email}}</td>
             <td class="px-4 py-3 text-xs border">
                 <select value="status" class="bg-transparent">
-                    <option value="applied">Applied</option>
-                    <option value="resume_viewed">Resume Viewed</option>
-                    <option value="under_consideration">Under Consideration</option>
-                    <option value="rejected">Rejected</option>
-                    <option value="selected">Selected</option>
+                    <option value="Just_Applied">Applied</option>
+                    <option value="ResumeViewed">Resume Viewed</option>
+                    <option value="Underconsideration">Under Consideration</option>
+                    <option value="Rejected">Rejected</option>
+                    <option value="Selected">Selected</option>
                 </select>
             </td>
-            <td class="px-4 py-3 text-sm border">6/4/2000</td>
+            <td class="px-4 py-3 text-sm border">{{formatDate(applicant.created_at)}}</td>
           </tr>
-          <tr class="text-gray-700">
-            <td class="px-4 py-3 border">
-              <div class="flex items-center text-sm">
-                <div class="relative w-8 h-8 mr-3 rounded-full">
-                  <img class="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                  <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                </div>
-                <div>
-                  <p class="font-semibold text-black">Stevens</p>
-                  <p class="text-xs text-gray-600">Programmer</p>
-                </div>
-              </div>
-            </td>
-            <td class="px-4 py-3 text-md font-semibold border">steves@example.com</td>
-            <td class="px-4 py-3 text-xs border">
-                <select value="status" class="bg-transparent">
-                    <option value="applied">Applied</option>
-                    <option value="resume_viewed">Resume Viewed</option>
-                    <option value="under_consideration">Under Consideration</option>
-                    <option value="rejected">Rejected</option>
-                    <option value="selected">Selected</option>
-                </select>
-            </td>
-            <td class="px-4 py-3 text-sm border">6/10/2020</td>
-          </tr>
-          <tr class="text-gray-700">
-            <td class="px-4 py-3 border">
-              <div class="flex items-center text-sm">
-                <div class="relative w-8 h-8 mr-3 rounded-full">
-                  <img class="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                  <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                </div>
-                <div>
-                  <p class="font-semibold">Nora</p>
-                  <p class="text-xs text-gray-600">Designer</p>
-                </div>
-              </div>
-            </td>
-            <td class="px-4 py-3 text-md font-semibold border">nora@example.com</td>
-            <td class="px-4 py-3 text-xs border">
-                <select value="status" class="bg-transparent">
-                    <option value="applied">Applied</option>
-                    <option value="resume_viewed">Resume Viewed</option>
-                    <option value="under_consideration">Under Consideration</option>
-                    <option value="rejected">Rejected</option>
-                    <option value="selected">Selected</option>
-                </select>
-            </td>
-            <td class="px-4 py-3 text-sm border">6/10/2020</td>
-          </tr>
-          <tr class="text-gray-700">
-            <td class="px-4 py-3 border">
-              <div class="flex items-center text-sm">
-                <div class="relative w-8 h-8 mr-3 rounded-full">
-                  <img class="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                  <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                </div>
-                <div>
-                  <p class="font-semibold">Ali</p>
-                  <p class="text-xs text-gray-600">Programmer</p>
-                </div>
-              </div>
-            </td>
-            <td class="px-4 py-3 border text-md font-semibold">ali@example.com</td>
-            <td class="px-4 py-3 border text-xs">
-                <select value="status" class="bg-transparent">
-                    <option value="applied">Applied</option>
-                            <option value="resume_viewed">Resume Viewed</option>
-                            <option value="under_consideration">Under Consideration</option>
-                            <option value="rejected">Rejected</option>
-                            <option value="selected">Selected</option>
-                    </select>
-            </td>
-            <td class="px-4 py-3 border text-sm">6/10/2020</td>
-          </tr>
-          <tr class="text-gray-700">
-            <td class="px-4 py-3 border">
-              <div class="flex items-center text-sm">
-                <div class="relative w-8 h-8 mr-3 rounded-full">
-                  <img class="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                  <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                </div>
-                <div>
-                  <p class="font-semibold">Khalid</p>
-                  <p class="text-xs text-gray-600">Designer</p>
-                </div>
-              </div>
-            </td>
-            <td class="px-4 py-3 border text-md font-semibold">khalid@example.com</td>
-            <td class="px-4 py-3 border text-xs">
-                <select value="status" class="bg-transparent">
-                    <option value="applied">Applied</option>
-                    <option value="resume_viewed">Resume Viewed</option>
-                    <option value="under_consideration">Under Consideration</option>
-                    <option value="rejected">Rejected</option>
-                    <option value="selected">Selected</option>
-                </select>
-            </td>
-            <td class="px-4 py-3 border text-sm">6/10/2020</td>
-          </tr>
-          <tr class="text-gray-700">
-            <td class="px-4 py-3 border">
-              <div class="flex items-center text-sm">
-                <div class="relative w-8 h-8 mr-3 rounded-full">
-                  <img class="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                  <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                </div>
-                <div>
-                  <p class="font-semibold">Nasser</p>
-                  <p class="text-xs text-gray-600">Pen Tester</p>
-                </div>
-              </div>
-            </td>
-            <td class="px-4 py-3 border text-md font-semibold">pen@example.com</td>
-            <td class="px-4 py-3 border text-xs">
-                <select value="status" class="bg-transparent">
-                    <option value="applied">Applied</option>
-                    <option value="resume_viewed">Resume Viewed</option>
-                    <option value="under_consideration">Under Consideration</option>
-                    <option value="rejected">Rejected</option>
-                    <option value="selected">Selected</option>
-                </select>
-            </td>
-            <td class="px-4 py-3 border text-sm">6/10/2020</td>
-          </tr>
-          <tr class="text-gray-700">
-            <td class="px-4 py-3 border">
-              <div class="flex items-center text-sm">
-                <div class="relative w-8 h-8 mr-3 rounded-full">
-                  <img class="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                  <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                </div>
-                <div>
-                  <p class="font-semibold">Mohammed</p>
-                  <p class="text-xs text-gray-600">Web Designer</p>
-                </div>
-              </div>
-            </td>
-            <td class="px-4 py-3 border text-md font-semibold">mohammed@example.com</td>
-            <td class="px-4 py-3 border text-xs">
-                <select value="status" class="bg-transparent">
-                    <option value="applied">Applied</option>
-                    <option value="resume_viewed">Resume Viewed</option>
-                    <option value="under_consideration">Under Consideration</option>
-                    <option value="rejected">Rejected</option>
-                    <option value="selected">Selected</option>
-    
-                </select>
-            </td>
-            <td class="px-4 py-3 border text-sm">6/10/2020</td>
-          </tr>
-          <tr class="text-gray-700">
-            <td class="px-4 py-3 border">
-              <div class="flex items-center text-sm">
-                <div class="relative w-8 h-8 mr-3 rounded-full">
-                  <img class="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                  <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                </div>
-                <div>
-                  <p class="font-semibold">Saad</p>
-                  <p class="text-xs text-gray-600">Data</p>
-                </div>
-              </div>
-            </td>
-            <td class="px-4 py-3 border text-md font-semibold">saad@example.com</td>
-            <td class="px-4 py-3 border text-xs">
-                <select value="status" class="bg-transparent">
-                            <option value="applied">Applied</option>
-                            <option value="resume_viewed">Resume Viewed</option>
-                            <option value="under_consideration">Under Consideration</option>
-                            <option value="rejected">Rejected</option>
-                            <option value="selected">Selected</option>
-                        </select>
-                </td>
-            <td class="px-4 py-3 border text-sm">6/10/2020</td>
-          </tr>
-          <tr class="text-gray-700">
-            <td class="px-4 py-3 border">
-              <div class="flex items-center text-sm">
-                <div class="relative w-8 h-8 mr-3 rounded-full">
-                  <img class="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                  <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                </div>
-                <div>
-                  <p class="font-semibold">Sami</p>
-                  <p class="text-xs text-gray-600">Developer</p>
-                </div>
-              </div>
-            </td>
-            <td class="px-4 py-3 border text-md font-semibold">sami@example.com</td>
-            <td class="px-4 py-3 border text-xs">
-                <select value="status" class="bg-transparent">
-                            <option value="applied">Applied</option>
-                            <option value="resume_viewed">Resume Viewed</option>
-                            <option value="under_consideration">Under Consideration</option>
-                            <option value="rejected">Rejected</option>
-                            <option value="selected">Selected</option>
-                        </select>
-            </td>
-            <td class="px-4 py-3 border text-sm">6/10/2020</td>
-          </tr>
+          <!--  -->
         </tbody>
       </table>
     </div>
@@ -253,4 +56,34 @@
 </template>
 <script setup>
     import AppHeader from '@/components/AppHeader.vue'
+    import { onMounted, ref } from 'vue'
+    import axios from 'axios'
+
+    import { useStore } from 'vuex'
+    import { useRouter } from 'vue-router'
+    const store = useStore()
+    const router = useRouter()
+    const applicants = ref()
+    const apiProgress = ref(true)
+
+    const formatDate = (dateString) => {
+      const date = new Date(dateString);
+      return date.toLocaleDateString(); // Adjust formatting as needed
+    };
+
+    onMounted(async () => {
+      if (!store.getters.isLoggedIn) {
+        router.push('/login')
+      }
+      try {
+        const res = await axios.get('/api/employer/1/job/72')
+        // console.log(res)
+        applicants.value = res.data.users
+        // console.log(applicants.value)
+        apiProgress.value = false
+        // console.log(user.role)
+      } catch (error) {
+        console.log(error)
+      }
+    })
 </script>
