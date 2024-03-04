3ec350a21d910aaae8c4b1c8351ba55b8393238b
ishmeetk01 <ishmeet0105@gmail.com>
2024-03-04 12:45:54 +0530

    View all notifications

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -90,44 +90,46 @@
             </div>
             
             <div v-else class="flex mt-5 lg:mt-0 lg:items-center">
-              <div v-if="store.getters.isRole==='candidate'" class="mt-12 lg:mt-0 lg:mr-10">
-                <button type="button"
-                  @click="openNotifMenu = !openNotifMenu"
-                  class="w-10 h-10 hover:ring flex items-center justify-center rounded-full text-white text-sm font-semibold border-none outline-none bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-600 ring-indigo-200">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20px" class="cursor-pointer fill-[#fff]"
-                    viewBox="0 0 371.263 371.263">
-                    <path
-                      d="M305.402 234.794v-70.54c0-52.396-33.533-98.085-79.702-115.151.539-2.695.838-5.449.838-8.204C226.539 18.324 208.215 0 185.64 0s-40.899 18.324-40.899 40.899c0 2.695.299 5.389.778 7.964-15.868 5.629-30.539 14.551-43.054 26.647-23.593 22.755-36.587 53.354-36.587 86.169v73.115c0 2.575-2.096 4.731-4.731 4.731-22.096 0-40.959 16.647-42.995 37.845-1.138 11.797 2.755 23.533 10.719 32.276 7.904 8.683 19.222 13.713 31.018 13.713h72.217c2.994 26.887 25.869 47.905 53.534 47.905s50.54-21.018 53.534-47.905h72.217c11.797 0 23.114-5.03 31.018-13.713 7.904-8.743 11.797-20.479 10.719-32.276-2.036-21.198-20.958-37.845-42.995-37.845a4.704 4.704 0 0 1-4.731-4.731zM185.64 23.952c9.341 0 16.946 7.605 16.946 16.946 0 .778-.12 1.497-.24 2.275-4.072-.599-8.204-1.018-12.336-1.138-7.126-.24-14.132.24-21.078 1.198-.12-.778-.24-1.497-.24-2.275.002-9.401 7.607-17.006 16.948-17.006zm0 323.358c-14.431 0-26.527-10.3-29.342-23.952h58.683c-2.813 13.653-14.909 23.952-29.341 23.952zm143.655-67.665c.479 5.15-1.138 10.12-4.551 13.892-3.533 3.773-8.204 5.868-13.353 5.868H59.89c-5.15 0-9.82-2.096-13.294-5.868-3.473-3.772-5.09-8.743-4.611-13.892.838-9.042 9.282-16.168 19.162-16.168 15.809 0 28.683-12.874 28.683-28.683v-73.115c0-26.228 10.419-50.719 29.282-68.923 18.024-17.425 41.498-26.887 66.528-26.887 1.198 0 2.335 0 3.533.06 50.839 1.796 92.277 45.929 92.277 98.325v70.54c0 15.809 12.874 28.683 28.683 28.683 9.88 0 18.264 7.126 19.162 16.168z"
-                      data-original="#000000"></path>
+              <div v-if="store.getters.isRole==='candidate'" class="mt-12 lg:mt-0 lg:mr-10 lg:pt-1">
+                
+                <button class="inline-block relative"
+                @click="notifMenu()">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                   </svg>
-                </button>
+                  <span class="animate-ping absolute top-1 right-0.5 block h-1 w-1 rounded-full ring-2 ring-purple-400 bg-purple-600"></span>
+              </button>
                 
                 <div
                   v-if="openNotifMenu"
-                  class="drop-down w-[410px] max-h-[400px] overflow-auto bg-white rounded-md shadow absolute top-12 lg:mr-10 lg:right-12"
+                  class="drop-down w-[380px] max-h-[400px] overflow-auto flex flex-col items-center bg-white rounded-md shadow absolute top-12 lg:mr-10 lg:right-12"
                 >
-                <div class="flex items-center justify-between my-4 px-4">
+                <!-- <div class="flex items-center justify-between my-4 px-8">
                   <p class="text-xs text-blue-500 cursor-pointer">Clear all</p>
-                </div>
+                </div> -->
                 <ul class="divide-y">
-                  <li 
-                  v-for="notification in notifications"
-                  :key="notification.id"
-                  class='py-4 px-4 flex items-center hover:bg-gray-50 text-black text-sm cursor-pointer'>
+                  <li v-for="(notification, index) in notificationsToShow" 
+                  :key="index" 
+                  class="py-4 px-2 flex items-center hover:bg-gray-50 text-black text-sm cursor-pointer">
                   <!-- <router-link :to="`/job/${notification.job_id}/apply`"> -->
-                    <img src="https://readymadeui.com/profile_2.webp" class="w-12 h-12 rounded-full shrink-0" />
+                    <!-- <img src="https://readymadeui.com/profile_2.webp" class="w-12 h-12 rounded-full shrink-0" /> -->
                     <div class="ml-6">
-                      <h3 class="text-sm text-[#333] font-semibold">Your have a new message from </h3>
+                      <h3 class="text-sm text-[#333] font-semibold">Your have a new message, for the job role {{ notification.data.job_title }} </h3>
                       <p class="text-xs text-gray-700 mt-2">{{notification.data.message}}</p>
-                      <p class="text-xs text-blue-500 leading-3 mt-2">{{notification.created_at}}</p>
+                      <p class="text-xs text-blue-500 leading-3 mt-2">{{notification.data.created_at}}</p>
                     </div>
                   <!-- </router-link> -->
                   </li>
                 </ul>
+                <p v-if="!view"
+                class="text-sm px-4 mt-6 mb-4 inline-block text-blue-500 cursor-pointer items-center"
+                @click="viewAll()">
+                  View all Notifications
+                </p>
               </div>
               </div>
               <div
-                @click="openUserMenu = !openUserMenu"
+                @click="userMenu()"
                 class="h-10 w-10 hover:ring-4 user cursor-pointer relative ring-blue-700/30 rounded-full bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80')]"
               >
                 <div
@@ -212,7 +214,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useStore } from 'vuex'
 import { toast } from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
@@ -224,17 +226,58 @@ const route = useRouter()
 
 const openUserMenu = ref(false)
 const openNotifMenu = ref(false)
-const notifications = ref()
+const notifications = ref([])
 const apiProgress = ref(true)
+const view = ref(false)
+const newNotif = ref(false)
+onMounted(async () => {
+  
+  try {
+    const res = await axios.get(`/api/user/${store.getters.User.id}/notifications`)
+    // console.log(res)
+    notifications.value = res.data.notifications
+    const oldNotificationLength = notifications.value.length
+    if (notifications.value.length > oldNotificationLength) {
+      newNotif.value = true
+    }
+    // console.log(notifications.value)
+    apiProgress.value = false
+    // console.log(user.role)
+  } catch (error) {
+    console.log(error)
+  }
+})
 
-const toggleMenu = function(){
-    if (openUserMenu.value ==true){
-      openNotifMenu.value = false
-    }
-    else if(openNotifMenu.value == true){
-      openUserMenu.value = false
-    }
+
+const viewAll = function(){
+  view.value = !view.value
 }
+
+const notificationsToShow = computed(() => {
+  if (view.value) {
+    return notifications.value
+  } else {
+    return notifications.value.slice(0, 5)
+  }
+})
+
+const notifMenu = function(){
+  openNotifMenu.value = !openNotifMenu.value
+  if (!openNotifMenu.value){
+    view.value = false
+  }
+  else{
+    openUserMenu.value = false
+  }
+}
+
+const userMenu = function(){
+  openUserMenu.value = !openUserMenu.value
+  if (openUserMenu.value){
+    openNotifMenu.value = false
+  }
+}
+
 const handleLogout = async () => {
   try {
     await store.dispatch('logout')
@@ -257,17 +300,4 @@ const handleLogout = async () => {
   }
 }
 
-onMounted(async () => {
-  
-  try {
-    const res = await axios.get(`/api/user/${store.getters.User.id}/notifications`)
-    console.log(res)
-    notifications.value = res.data.notifications
-    console.log(notifications.value)
-    apiProgress.value = false
-    // console.log(user.role)
-  } catch (error) {
-    console.log(error)
-  }
-})
 </script>
