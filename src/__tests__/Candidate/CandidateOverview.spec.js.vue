0eb3be0efaf459fb32a5c6b3569c40965248683b
ishmeetk01 <ishmeet0105@gmail.com>
2024-03-06 15:29:46 +0530

    Added tests

---
+++ src/__tests__/Candidate/CandidateOverview.spec.js.vue
@@ -0,0 +1,19 @@
+import { shallowMount } from "@vue/test-utils";
+import CandidateOverview from "@/views/mocked/CandidateOverview.vue";
+import { describe, it , expect} from "vitest";
+import UserNavigation from '@/components/UserNavigation.vue';
+import { Form } from "vee-validate";
+
+//import axios from '../../api.js';
+describe('CandidateOverview',() => {
+  it('Candidate Overview', async() => {
+    
+  
+    const wrapper = shallowMount(CandidateOverview);
+    let text = 'Candidate Overview'
+    expect(wrapper.text()).toMatch(text)
+    
+    expect(wrapper.findComponent(UserNavigation).exists( )).toBeTruthy();
+    
+    const form =  wrapper.findComponent(Form)
+    expect(form.exists()).toBe(true)
